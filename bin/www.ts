9555a52da9b4e3b0c4e990457d1e8a8ad32a2874
Juhyung Park <jh@kodebox.io>
2018-08-14 19:07:06 +0900

    Change default port to 3003

--- bin/www.ts
+++ bin/www.ts
@@ -18,7 +18,7 @@ async function main() {
          * Get port from environment and store in Express.
          */
 
-        const port = normalizePort(process.env.PORT || "3000");
+        const port = normalizePort(process.env.PORT || "3003");
         app.set("port", port);
 
         /**
