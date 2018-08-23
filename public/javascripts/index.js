2e787760df8c57fecd2f80138fa088d8cdb2031c
Hyunsik Jeong <rbtree@kodebox.io>
2018-08-23 18:35:36 +0900

    Added a Tweet button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,14 @@ $(document).ready(function () {
         $("#sns-button").prop('disabled', true);
         grecaptcha.execute();
     });
+    $("#tweet-button").click(() => {
+        var w = 600, h = 400;
+        var l = screen.width/2 - w/2;
+        var t = screen.height/2 - h/2;
+        var url = "https://twitter.com/intent/tweet?text=I+love+CodeChain%0A%3CYour+Platform+Account+Address+Here%3E%0AHusky CodeChain Testnet&via=codechain_io&url=https%3A%2F%2Fhusky.codechain.io";
+        var default_option = "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no";
+        window.open(url, "popup", default_option + ", width=" + w + ",height=" + h + ",top=" + t + ",left=" + l); 
+    });
 });
 
 function onCaptchaSuccess(captchaResult) {
